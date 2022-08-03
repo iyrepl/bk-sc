1b22858ec8b020f6257b7ac06c1fe6dfdf6d26b9
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-03 20:46:45 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 
-# rm -rf brook_linux_amd64 #Uncomment this line to update
+# rm -rf brook #Uncomment this line to update
 
-if [ ! -f "brook_linux_amd64" ];then
-  curl -L https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64
+if [ ! -f "brook" ];then
+  curl -L https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -o brook
 fi
-chmod +x brook_linux_amd64
+chmod +x brook
 
-./brook_linux_amd64 wsserver --listen :"$PORT" --password "$passwd"
+./brook wsserver --listen :"$PORT" --password "$passwd"
