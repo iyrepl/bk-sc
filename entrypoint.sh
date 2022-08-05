b6a4829e5fb239804f57dd1284dc068844e538bc
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-05 21:57:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@
 wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
 chmod +x /usr/local/bin/brook
 
-# Run brook
-/usr/local/bin/brook wsserver --listen :${PORT} --password ${passwd}
+# Run brook & nginx
+/usr/local/bin/brook wsserver --listen :${PORT} --password ${passwd} && "/usr/local/nginx/sbin/nginx", "-g", "daemon off;"
