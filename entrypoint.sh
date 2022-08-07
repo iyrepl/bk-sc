9a3a61357ea71802aaeba80ef946112f3bd24af0
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-07 20:32:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ mv PanIndex-linux-amd64 /usr/local/bin/panindex
 rm -f panindex.tar.gz & rm -f LICENSE
 chmod +x /usr/local/bin/panindex
 
-# Download and install brook
+# Download brook
 wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
 chmod +x /usr/local/bin/brook
 
 # Run panindex & brook & nginx
 
-./usr/local/bin/panindex &
+/usr/local/bin/panindex &
 
 /usr/local/bin/brook wsserver --listen :1080 --path /lockey --password ${passwd} &
 
