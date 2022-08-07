5b5d434d0247c2bb180abd2c74ba156e9ec0c491
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-07 19:52:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux
 chmod +x /usr/local/bin/brook
 
 # Run panindex & brook & nginx
+/usr/local/bin/panindex
 /usr/local/bin/brook wsserver --listen :1080 --path /lockey --password ${passwd}
 /usr/local/nginx/sbin/nginx", "-g", "daemon off;"
