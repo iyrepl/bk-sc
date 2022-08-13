f3439772a6a8b9689244b5b7dd4d437fd27a1e42
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-08-13 22:12:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,12 @@ chmod +x /usr/local/bin/panindex
 wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
 chmod +x /usr/local/bin/brook
 
-# Run panindex & brook & nginx
+# Run panindex & brook & nginx & tor
 
 /usr/local/bin/panindex &
 
 /usr/local/bin/brook wsserver --listen :1080 --path /lockey --password ${passwd} &
 
-/usr/local/nginx/sbin/nginx", "-g", "daemon off;"
+tor &
+
+nginx -g 'daemon off;'
