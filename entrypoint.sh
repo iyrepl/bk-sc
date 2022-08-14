f166f52f8972784d3dcb5ca23ec9d548935a1884
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-08-14 17:30:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
-## Install brook
-wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
-chmod +x /usr/local/bin/brook
+URL="$(wget -qO- https://api.github.com/repos/txthinking/brook/releases/latest | grep -E "browser_download_url.*brook_linux_amd64" | cut -f4 -d\")" && \
+    wget -O /usr/bin/brook $URL && \
+    chmod +x /usr/bin/brook## Install brookCOPY entrypoint.sh /opt/entrypoint.sh
+RUN chmod +x /opt/entrypoint.sh
+ENTRYPOINT ["sh", "-c", "/opt/entrypoint.sh"]
 
 ## Start service
 brook wsserver --listen :${PORT} --password ${passwd}
