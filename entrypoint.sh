ee356a39697a3fdf93fb4163044c670b044fdbee
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-08-14 16:30:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Download brook
-wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
-chmod +x /usr/local/bin/brook
+#wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
+#chmod +x /usr/local/bin/brook
 
 # Run brook & nginx & tor
 
