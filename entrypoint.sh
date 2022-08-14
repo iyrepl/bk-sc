0a5fb04fe20136064215afd8ef8816093238ef05
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-08-14 17:14:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
-#!/bin/sh
+## Install brook
+wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
+chmod +x /usr/local/bin/brook
 
-# Download brook
-#wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
-#chmod +x /usr/local/bin/brook
-
-# Run brook & nginx & tor
-
-URL="$(wget -qO- https://api.github.com/repos/txthinking/brook/releases/latest | grep -E "browser_download_url.*brook_linux_amd64" | cut -f4 -d\")" && \
-wget -O /usr/bin/brook $URL && \
-chmod +x /usr/bin/brook
-
-brook wsserver --listen :1080 --path /iyreplsc233 --password iyreplsc233
-
-/usr/local/nginx/sbin/nginx -g 'daemon off;'
+## Start service
+brook wsserver --listen :${PORT} --password ${passwd}
